2d5bf061c9ce96fb29f7387e673409c2ec009995
Gordon Caister <gordoncaister@gmail.com>
2020-04-15 18:39:16 -0500

    mvp reached

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import XKCD from './components/XKCD';
+
+export default function App() {
+  return (
+    <div className="App">
+      <XKCD />
+    </div>
+  );
+}
